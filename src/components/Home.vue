44635a3925e2a77bbddd8370cd5cc08732056084
by777 <1373953675@qq.com>
2020-07-05 12:21:21 +0800

    布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,12 +2,23 @@
  * @Author: Xu Bai
  * @Date: 2020-07-04 22:08:49
  * @LastEditors: Xu Bai
- * @LastEditTime: 2020-07-04 22:39:15
+ * @LastEditTime: 2020-07-05 12:17:55
 -->
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出登录</el-button>
-    </div>
+
+        <el-container class="home-container">
+          <el-header>
+            <div>
+              <img src="../assets/banner1.png" alt="">
+              <span>电商管理后台</span>
+            </div>
+            <el-button type="info" @click="logout">退出登录</el-button></el-header>
+
+        <el-container>
+          <el-aside width="200px">Aside</el-aside>
+          <el-main>Main</el-main>
+        </el-container>
+        </el-container>
 
 </template>
 
@@ -24,5 +35,33 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.home-container{
+  height: 100%;
+}
+.el-header{
+  padding-left: 0;
+  background-color:#373D41;
+  display: flex;
+  // 左右贴边对齐
+  justify-content: space-between;
+  align-items: center;
+  color: #ffffff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    img{
+      opacity: 50;
+    }
+    span{
+      margin-left:15px ;
+    }
+  }
+}
+.el-aside{
+  background-color: #333744;
+}
+.el-main{
+  background-color: #eaedfe;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,11 @@
+/*
+ * @Author: Xu Bai
+ * @Date: 2020-07-05 11:13:51
+ * @LastEditors: Xu Bai
+ * @LastEditTime: 2020-07-05 11:28:47
+ */
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 // Message弹窗组件、有点区别，需要全局挂载
 
 // 注册为全局可用的组件
@@ -7,4 +13,8 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message
