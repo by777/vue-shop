7c1ed33ca1ccdbdbe4a4f66d8c894b73ec209791
by777 <1373953675@qq.com>
2020-07-12 21:59:26 +0800

    商品表格

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -2,7 +2,7 @@
  * @Author: Xu Bai
  * @Date: 2020-07-12 21:22:39
  * @LastEditors: Xu Bai
- * @LastEditTime: 2020-07-12 21:30:48
+ * @LastEditTime: 2020-07-12 21:57:39
 -->
 <template>
     <div>
@@ -18,14 +18,50 @@
                 <el-col><el-button type="primary">添加分类</el-button></el-col>
             </el-row>
             <!-- 表格 -->
+            <tree-table :data="catelist" :columns="columns" :selection-type="false" :expand-type="false"
+            show-index index-text="#" border
+            >
 
+            </tree-table>
             <!-- 分页区域 -->
         </el-card>
     </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      // 商品分类的数据列表
+      catelist: [],
+      // 总数据条数
+      total: 0,
+      // 查询条件
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 为table指定列的定义
+      columns: [{
+        label: '分类名称',
+        prop: 'cat_name'
+      }]
+    }
+  },
+  methods: {
+    // 获取商品分类数据
+    async getCateList () {
+      const { data: res } = await this.$http.get('categories', { params: this.queryInfo })
+      if (res.meta.status !== 200) return this.$message.error('本次获取商品分类失败')
+      // 保存数据列表
+      this.catelist = res.data.result
+      this.total = res.data.total
+      console.log(this.catelist)
+    }
+  },
+  created () {
+    this.getCateList()
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: Xu Bai
  * @Date: 2020-07-03 23:21:38
  * @LastEditors: Xu Bai
- * @LastEditTime: 2020-07-05 13:22:15
+ * @LastEditTime: 2020-07-12 21:48:53
  */
 import Vue from 'vue'
 import App from './App.vue'
@@ -10,6 +10,7 @@ import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // config就是请求对象，header头条件字段
 axios.interceptors.request.use(config => {
@@ -21,6 +22,7 @@ Vue.prototype.$http = axios
 
 // 每个vue的组件都能直接通过$http直接访问
 Vue.config.productionTip = false
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
