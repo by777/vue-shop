c826cf8e722e028d8c5fb9a2f7d44426e7857171
by777 <1373953675@qq.com>
2020-08-09 22:47:24 +0800

    p146

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -2,7 +2,7 @@
  * @Author: Xu Bai
  * @Date: 2020-08-08 21:30:06
  * @LastEditors: Xu Bai
- * @LastEditTime: 2020-08-08 21:41:29
+ * @LastEditTime: 2020-08-09 22:44:09
 -->
 <template>
     <div>
@@ -24,6 +24,25 @@
                   <el-button type="primary">添加商品</el-button>
               </el-col>
             </el-row>
+            <!-- table表格 -->
+            <el-table :data="goodslist" border stripe >
+              <el-table-column label="#" type="index"></el-table-column>
+              <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+              <el-table-column label="商品价格/元" prop="goods_price" width="70px"></el-table-column>
+              <el-table-column label="商品重量/kg" prop="goods_weight" width="70px"></el-table-column>
+              <el-table-column label="创建时间" prop="add_time" width="150px">
+                <template slot-scope="scope">
+                  {{scope.row.add_time | dateFormat}}
+                </template>
+              </el-table-column>
+              <el-table-column label="操作" width="180px">
+                <template slot-scope="scope">
+                  <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+                  <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+                    {{scope.row.goods_id}}
+                </template>
+              </el-table-column>
+            </el-table>
         </el-card>
     </div>
 </template>
@@ -32,13 +51,34 @@
 export default {
 
   data () {
-    return {}
+    return {
+      // 查询参数对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      // 请求返回的GOODS
+      goodslist: [],
+      // 总数据条数
+      total: 0
+
+    }
   },
   created () {
-
+    this.getGoodsList()
   },
   methods: {
-
+    // 分页获取商品列表
+    async getGoodsList () {
+      const { data: res } = await this.$http.get('goods', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) return this.$message.error('获取商品列表失败！')
+      // console.log(res.data)
+      this.goodslist = res.data.goods
+      this.total = res.data.total
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: Xu Bai
  * @Date: 2020-07-03 23:21:38
  * @LastEditors: Xu Bai
- * @LastEditTime: 2020-07-12 21:48:53
+ * @LastEditTime: 2020-08-09 22:41:03
  */
 import Vue from 'vue'
 import App from './App.vue'
@@ -23,6 +23,18 @@ Vue.prototype.$http = axios
 // 每个vue的组件都能直接通过$http直接访问
 Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
+// 全局过滤器
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
